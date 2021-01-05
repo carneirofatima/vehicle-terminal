0edab8c95a051ae475893159f09b0b2210060fd8
Fatima Carneiro <fatima.carneiro@wavy.global>
2021-01-05 10:52:29 -0300

    Spring Initializr

---
+++ src/main/kotlin/com/carneirofatima/vehicleterminal/VehicleTerminalApplication.kt
@@ -0,0 +1,11 @@
+package com.carneirofatima.vehicleterminal
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VehicleTerminalApplication
+
+fun main(args: Array<String>) {
+	runApplication<VehicleTerminalApplication>(*args)
+}
---
+++ src/test/kotlin/com/carneirofatima/vehicleterminal/VehicleTerminalApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.carneirofatima.vehicleterminal
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VehicleTerminalApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
